c4978b3569952dc790eba1df36cd5e004c9e8123
Tim Swast <tswast@gmail.com>
2018-04-29 10:54:13 -0700

    Rename frames table to image. Rename add-frames to import-clip.
    Adds new table called "frame" to hold reference to image used in an
    animation clip. Adds table "clip" to hold animations.

--- pikov/pikov.py
+++ pikov/pikov.py
@@ -29,37 +29,51 @@ class Pikov(object):
         connection = sqlite3.connect(path)
         return cls(connection)
 
-    @property
-    def size(self):
-        cursor = self.connection.cursor()
-        cursor.execute('SELECT frame_width, frame_height FROM pikov')
-        return cursor.fetchone()
-
-    def add_frame(self, frame):
-        """Add a frame image to the Pikov file.
+    def add_image(self, image):
+        """Add an image to the Pikov file.
 
         Args:
-            frame (PIL.Image.Image):
-                A frame image to add to the Pikov file.
+            image (PIL.Image.Image):
+                An image to add to the Pikov file.
 
         Returns:
-            bool:
-                True if the image was added. False if the the image was a
-                duplicate of an existing frame.
+            Tuple[str, bool]:
+                The content-based address to the image and a boolean
+                indicating True if the image was added or False for
+                duplicates.
         """
         image_io = io.BytesIO()
-        frame.save(image_io, format='PNG')
-        image_hash = hash_image(frame)
+        image.save(image_io, format='PNG')
+        image_hash = hash_image(image)
 
         try:
             with self.connection:
                 cursor = self.connection.cursor()
                 cursor.execute(
-                    'INSERT INTO frames (key, contents) '
+                    'INSERT INTO image (key, contents) '
                     'VALUES (?, ?)', (image_hash, image_io.read()))
         except sqlite3.IntegrityError:
-            return False  # Frame already exists
-        return True
+            return image_hash, False  # Frame already exists
+        return image_hash, True
+
+
+def is_blank(image):
+    """Determine if an image is blank.
+
+    Args:
+        frame (PIL.Image.Image):
+            A frame image to add to the Pikov file.
+
+    Returns:
+        bool:
+            True if the image contains only a single solid color.
+    """
+    solid_color = image.getpixel((0, 0))
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            if solid_color != image.getpixel((x, y)):
+                return False
+    return True
 
 
 def hash_image(image):
@@ -83,13 +97,13 @@ def hash_image(image):
     return md5.hexdigest()
 
 
-def add_frames(pikov_path, sprite_sheet_path):
+def import_clip(pikov_path, sprite_sheet_path, frame_width, frame_height):
     frames_added = 0
     duplicates = 0
+    blanks = 0
 
-    # Read the frame size from the Pikov file.
+    # Read the Pikov file.
     pikov = Pikov.open(pikov_path)
-    frame_width, frame_height = pikov.size
 
     # Chop the sprite sheet into frames.
     sheet = Image.open(sprite_sheet_path)
@@ -102,26 +116,34 @@ def add_frames(pikov_path, sprite_sheet_path):
                 col * frame_width, row * frame_height,
                 (col + 1) * frame_width, (row + 1) * frame_height,))
 
-            if pikov.add_frame(frame):
+            if is_blank(frame):
+                blanks += 1
+                continue
+
+            key, added = pikov.add_image(frame)
+            if added:
                 frames_added += 1
             else:
                 duplicates += 1
 
-    print('Added {} of {} frames ({} duplicates)'.format(
-        frames_added, rows * cols, duplicates))
+    print('Added {} of {} frames ({} duplicates, {} blank)'.format(
+        frames_added, rows * cols, duplicates, blanks))
 
 
-def create(pikov_path, frame_width, frame_height):
+def create(pikov_path):
     conn = sqlite3.connect(pikov_path)
     cursor = conn.cursor()
     cursor.execute(
-        'CREATE TABLE frames (key TEXT PRIMARY KEY, contents BLOB)')
+        'CREATE TABLE image (key TEXT PRIMARY KEY, contents BLOB)')
     cursor.execute(
-        'CREATE TABLE pikov '
-        '(id INTEGER PRIMARY KEY, frame_width INTEGER, frame_height INTEGER)')
+        'CREATE TABLE frame ('
+        'id INTEGER PRIMARY KEY, '
+        'image_key TEXT, '
+        'FOREIGN KEY(image_key) REFERENCES image(key))')
     cursor.execute(
-        'INSERT INTO pikov (frame_width, frame_height) VALUES (?, ?)',
-        (frame_width, frame_height))
+        'CREATE TABLE clip ('
+        'id INTEGER PRIMARY KEY, '
+        'sequence TEXT)')
     conn.commit()
 
 
@@ -132,22 +154,24 @@ def main():
     create_parser = subparsers.add_parser(
         'create', help='Create a new .pikov file.')
     create_parser.add_argument('pikov_path', help='Path to .pikov file.')
-    create_parser.add_argument(
-        'frame_size', help='Size of frame. WIDTHxHEIGHT. Example: 8x8')
 
-    add_frames_parser = subparsers.add_parser(
-        'add-frames',
-        help='Add frames from a sprite sheet to an existing .pikov file.')
-    add_frames_parser.add_argument('pikov_path', help='Path to .pikov file.')
-    add_frames_parser.add_argument(
+    import_clip_parser = subparsers.add_parser(
+        'import-clip',
+        help='Import a sprite sheet animation as a clip.')
+    import_clip_parser.add_argument('pikov_path', help='Path to .pikov file.')
+    import_clip_parser.add_argument(
         'sprite_sheet_path', help='Path to sprite sheet.')
+    import_clip_parser.add_argument(
+        'frame_size', help='Size of frame. WIDTHxHEIGHT. Example: 8x8')
 
     args = parser.parse_args()
     if args.action == 'create':
+        create(args.pikov_path)
+    elif args.action == 'import-clip':
         frame_width, frame_height = map(int, args.frame_size.split('x'))
-        create(args.pikov_path, frame_width, frame_height)
-    elif args.action == 'add-frames':
-        add_frames(args.pikov_path, args.sprite_sheet_path)
+        import_clip(
+            args.pikov_path, args.sprite_sheet_path, frame_width,
+            frame_height)
     elif args.action is not None:
         raise NotImplementedError(
             'Got unknown action: {}'.format(args.action))
