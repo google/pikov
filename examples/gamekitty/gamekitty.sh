c4526583dfb1c7491d503791a4ea3cd2eecd622a
Tim Swast <tswast@gmail.com>
2018-06-24 12:26:15 -0700

    Add sitting_bat clip to gamekitty example
    In sitting_bat, gamekitty is sitting down and bats his paw.
    
    Towards #1

--- examples/gamekitty/gamekitty.sh
+++ examples/gamekitty/gamekitty.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Copyright 2018 Google LLC
 #
@@ -14,34 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Animation clips, as defined in the PICO-8 version of gamekitty.
+# https://github.com/tswast/pixelsketches/blob/master/pico-8/gamekitty.p8
+clips=(
+    0
+    1,2,3,4
+    34,35
+    36,37,38,39,41,42,43,44
+    41,42,43,44
+    26,4
+    57,58,59,60,61
+    10,11,12,13,14,15,16
+    18,19,20,21,22,23,24,25
+    46,47,48,49
+    50,51,45,45,52,48,49
+    52,48,49
+    53,54,55
+    84,85,87,88,89,90,91,92,93,95,96,97
+    62,63,64,65,66
+    40
+)
 rm examples/gamekitty/gamekitty.pikov ; pikov create examples/gamekitty/gamekitty.pikov
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 0
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 1,2,3,4
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 34,35
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 36,37,38,39,41,42,43,44
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 41,42,43,44
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 26,4
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 57,58,59,60,61,62,63,64,65,66
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 10,11,12,13,14,15,16
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 18,19,20,21,22,23,24,25
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 46,47,48,49
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 50,51,45,45,52,48,49
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 52,48,49
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 53,54,55
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 84,85,87,88,89,90,91,92,93,95,96,97
-pikov import-clip examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 40
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 0
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 1,2,3,4
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 34,35
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 36,37,38,39,41,42,43,44
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 41,42,43,44
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 26,4
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 57,58,59,60,61,62,63,64,65,66
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 10,11,12,13,14,15,16
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 18,19,20,21,22,23,24,25
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 46,47,48,49
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 50,51,45,45,52,48,49
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 52,48,49
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 53,54,55
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 84,85,87,88,89,90,91,92,93,95,96,97
-pikov import-clip --flip_x=True examples/gamekitty/gamekitty.pikov examples/gamekitty/gamekitty.png 8x8 40
+
+for clip in $clips; do
+    pikov import-clip \
+        examples/gamekitty/gamekitty.pikov \
+        examples/gamekitty/gamekitty.png 8x8 $clip
+done
+
+# Mirror images
+for clip in $clips; do
+    pikov import-clip --flip_x=True \
+        examples/gamekitty/gamekitty.pikov \
+        examples/gamekitty/gamekitty.png 8x8 $clip
+done
