88321996e6fa94e81ba8a81abbdc03a7b10516dc
Tim Swast <swast@google.com>
2019-04-11 10:02:41 -0700

    Delete nodes without names
    They are causing the sample file to grow without bound. Add a script
    to remove them until I can track down where they are coming from.

--- python/pikov/json.py
+++ python/pikov/json.py
@@ -55,45 +55,66 @@ class JSONGraph(core.AbstractGraph):
             return core.Int64Node(int(value["int64"]))
 
     def get_value(self, source, label):
-        if source.guid not in self._guid_map:
+        source_guid = _get_guid(source)
+        label_guid = _get_guid(label)
+
+        if source_guid not in self._guid_map:
             return None
 
-        node_json = self._guid_map[source.guid]
-        if label.guid not in node_json:
+        node_json = self._guid_map[source_guid]
+        if label_guid not in node_json:
             return None
 
-        return self._from_json(node_json[label.guid])
+        return self._from_json(node_json[label_guid])
 
     def __iter__(self):
         for guid in self._guid_map:
             yield core.GuidNode(self, guid=guid)
 
+    def delete_node(self, node):
+        node_guid = _get_guid(node)
+        del self._guid_map[node_guid]
+
     def get_labels(self, node):
-        node_json = self._guid_map.get(node.guid, {})
+        node_guid = _get_guid(node)
+
+        node_json = self._guid_map.get(node_guid, {})
         for guid in node_json:
             yield core.GuidNode(self, guid=guid)
 
     def _unset_value(self, source, label):
-        if source.guid not in self._guid_map:
+        source_guid = _get_guid(source)
+        label_guid = _get_guid(label)
+
+        if source_guid not in self._guid_map:
             return
 
-        node_json = self._guid_map[source.guid]
-        if label.guid not in node_json:
+        node_json = self._guid_map[source_guid]
+        if label_guid not in node_json:
             return
 
-        del node_json[label.guid]
+        del node_json[label_guid]
 
     def set_value(self, source, label, target):
         """Create/update/remove an edge.
 
         Set the ``target`` value to ``None`` to remove an edge.
         """
+        source_guid = _get_guid(source)
+        label_guid = _get_guid(label)
+
         if target is None:
             self._unset_value(source, label)
             return
 
-        if source.guid not in self._guid_map:
-            self._guid_map[source.guid] = {}
+        if source_guid not in self._guid_map:
+            self._guid_map[source_guid] = {}
 
-        node_json = self._guid_map[source.guid]
-        node_json[label.guid] = self._to_json(target)
+        node_json = self._guid_map[source_guid]
+        node_json[label_guid] = self._to_json(target)
+
+
+def _get_guid(node_or_guid):
+    if isinstance(node_or_guid, str):
+        return node_or_guid
+    return node_or_guid.guid
